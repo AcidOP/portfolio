39d1084dd7b42055f20d9bc6d0dcb5023b0d645f
AcidOP <zeeshanalivr46@gmail.com>
2024-02-15 09:08:05 +0530

    fix: fixed UI breaking in desktop devices

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,10 +9,9 @@ const body = Inter({ subsets: ['latin'], weight: '700' });
 const Homepage = () => {
   return (
     <div className='flex h-[80vh] w-screen items-center lg:h-screen'>
-      <div className='relative mx-auto flex h-[30vh] w-full flex-col justify-between px-5 lg:h-2/5 lg:w-3/4'>
+      <div className='relative mx-auto flex h-[32vh] w-full flex-col justify-between px-5 lg:h-1/2 lg:w-3/4'>
         <div className={clsx('text-6xl lg:text-8xl', heading.className)}>
           <h1>
-            Hey,
             <span className='text-5xl opacity-90 lg:text-7xl'>Hey,</span>
             <br /> I&apos;m {config.name.split(/ /g)[0]}
           </h1>
@@ -20,7 +19,7 @@ const Homepage = () => {
 
         <div
           className={clsx(
-            'absolute bottom-0 right-5 max-w-[75%] text-lg opacity-75 lg:right-0 lg:max-w-3xl lg:text-4xl',
+            'absolute bottom-0 right-0 max-w-[75%] text-xl opacity-75 lg:right-0 lg:max-w-3xl lg:text-4xl',
             body.className,
           )}
         >
