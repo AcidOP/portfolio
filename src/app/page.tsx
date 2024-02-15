3e0cf065855977f13ccb3efe28010d22e19757f2
AcidOP <zeeshanalivr46@gmail.com>
2024-02-15 18:44:36 +0530

    feat: added an aesthetic blob in homepage ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,6 +9,10 @@ const body = Inter({ subsets: ['latin'], weight: '700' });
 const Homepage = () => {
   return (
     <div className='flex h-[70vh] w-full items-center justify-center lg:h-[calc(100vh-5rem)]'>
+      <div className='absolute top-0 -z-10 h-full w-full bg-white'>
+        <div className='absolute bottom-auto left-auto right-0 top-0 h-[500px] w-[500px] -translate-x-[30%] translate-y-[20%] rounded-full bg-violet-400 opacity-50 blur-[80px]' />
+      </div>
+
       <div className='relative flex h-[32vh] w-full flex-col justify-between px-5 lg:h-1/2 lg:w-5/6'>
         <div className={clsx('text-6xl lg:text-8xl', heading.className)}>
           <h1>
