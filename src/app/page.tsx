659ce7780c22b98a0f14ae87832031095bb6dee8
AcidOP <zeeshanalivr46@gmail.com>
2024-02-15 18:36:18 +0530

    fix: fixed misalignment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,8 +8,8 @@ const body = Inter({ subsets: ['latin'], weight: '700' });
 
 const Homepage = () => {
   return (
-    <div className='flex h-[80vh] w-screen items-center lg:h-screen'>
-      <div className='relative mx-auto flex h-[32vh] w-full flex-col justify-between px-5 lg:h-1/2 lg:w-3/4'>
+    <div className='flex h-[70vh] lg:h-[calc(100vh-5rem)] w-full items-center justify-center'>
+      <div className='relative flex h-[32vh] w-full flex-col justify-between px-5 lg:h-1/2 lg:w-5/6'>
         <div className={clsx('text-6xl lg:text-8xl', heading.className)}>
           <h1>
             <span className='text-5xl opacity-90 lg:text-7xl'>Hello,</span>
@@ -19,7 +19,7 @@ const Homepage = () => {
 
         <div
           className={clsx(
-            'absolute bottom-0 right-0 max-w-[75%] text-xl opacity-75 lg:right-0 lg:max-w-3xl lg:text-4xl',
+            'absolute bottom-0 right-0 max-w-[75%] text-xl opacity-75 lg:max-w-3xl lg:text-4xl',
             body.className,
           )}
         >
