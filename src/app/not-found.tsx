41f77afe1d9500b6eca7b3a84f7b38e44c1111d6
AcidOP <zeeshanalivr46@gmail.com>
2024-03-09 17:49:24 +0530

    feat: added a 404 page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,29 +1,40 @@
 import image404 from '@/../public/images/404/404.svg';
 import { cn } from '@/utils/cn';
 
+import { Suez_One } from 'next/font/google';
 import Image from 'next/image';
 import Link from 'next/link';
 
-import { Suez_One } from 'next/font/google';
 const suez = Suez_One({ subsets: ['latin'], weight: '400' });
 
 const NotFound = () => {
   return (
-    <div className='grid place-content-center h-[130vh]'>
+    <div className='grid h-[130vh] place-content-center'>
       <div className='absolute left-1/2 -z-10 h-[500px] w-[500px] -translate-x-1/2 -translate-y-[20%] rounded-full bg-violet-600 blur-[100px] lg:h-[800px] lg:w-[800px]' />
 
-      <Image src={image404} alt='404' className='absolute left-1/2 -z-10 h-[500px] w-[500px] -translate-x-1/2 -translate-y-[20%] rounded-full lg:h-[600px] lg:w-[600px]' />
+      <Image
+        src={image404}
+        alt='404'
+        className='absolute left-1/2 -z-10 h-[500px] w-[500px] -translate-x-1/2 -translate-y-[20%] rounded-full lg:h-[600px] lg:w-[600px]'
+      />
 
       <div>
-        <h1 className={cn('text-6xl lg:text-9xl mb-3 lg:text-white drop-shadow-lg', suez.className)}>Lost in space?</h1>
+        <h1
+          className={cn(
+            'mb-3 text-6xl drop-shadow-lg lg:text-9xl lg:text-white',
+            suez.className,
+          )}
+        >
+          Lost in space?
+        </h1>
 
         <Link
           href='/'
-          className='group relative grid w-min lg:mt-6 place-content-center overflow-hidden overflow-x-hidden border-2 border-black bg-black text-white mx-auto px-10 py-4'
+          className='group relative mx-auto grid w-min place-content-center overflow-hidden overflow-x-hidden border-2 border-black bg-black px-10 py-4 text-white lg:mt-6'
         >
           <span
             className={cn(
-              'relative z-10 text-nowrap text-3xl font-medium text-white group-hover:text-black transition-all duration-300 lg:text-6xl',
+              'relative z-10 text-nowrap text-3xl font-medium text-white transition-all duration-300 group-hover:text-black lg:text-6xl',
               suez.className,
             )}
           >
