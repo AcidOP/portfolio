f308e653faac1aaca3ee9651febb63b626427181
AcidOP <zeeshanalivr46@gmail.com>
2024-03-15 18:37:23 +0530

    feat: generate metadata for blogs

--- src/app/blogs/[slug]/page.tsx
+++ src/app/blogs/[slug]/page.tsx
@@ -4,6 +4,7 @@ import GoBackButton from './_components/GoBackButton';
 import Header from './_components/Header';
 
 import Mdx from '@/components/mdx/Mdx';
+import config from '@/data/config';
 import { getAllBlogSlugs, getBlogBySlug } from '@/utils/blog';
 
 import { notFound } from 'next/navigation';
@@ -31,17 +32,27 @@ export const generateMetadata = async ({
   return {
     title: blog.data.title,
     description: blog.data.description,
-    // openGraph: {
-    //   title: blog.data.title,
-    //   description: blog.data.description,
-    //   type: 'article',
-    //   url: process.env.SITE_URL + '/blogs/' + slug,
-    // },
-    // twitter: {
-    //   card: 'summary_large_image',
-    //   title: blog.data.title,
-    //   description: blog.data.description,
-    // },
+    openGraph: {
+      title: blog.data.title,
+      description: blog.data.description,
+      type: 'article',
+      url: process.env.NEXT_PUBLIC_SITE_URL + `/blogs/${slug}`,
+      authors: config.name,
+      images: [
+        {
+          url: blog.data.cover,
+          width: 1200,
+          height: 630,
+          alt: blog.data.title,
+        },
+      ],
+    },
+    twitter: {
+      card: 'summary_large_image',
+      title: blog.data.title,
+      description: blog.data.description,
+      images: [{ url: blog.data.cover }],
+    },
   };
 };
 
