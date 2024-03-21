94daa2eb81ea4f0277b499bc0797ed54669793c2
AcidOP <zeeshanalivr46@gmail.com>
2024-03-21 12:25:12 +0530

    feat: added hover ui in back button

--- src/app/blogs/[slug]/_components/back-button.tsx
+++ src/app/blogs/[slug]/_components/back-button.tsx
@@ -9,7 +9,10 @@ const GoBackButton: FC = () => {
   const previousUrl = router.back;
 
   return (
-    <button onClick={previousUrl} className='font-medium text-violet-600'>
+    <button
+      onClick={previousUrl}
+      className='rounded-md px-3 py-2 font-medium text-violet-600 transition-all duration-200 hover:bg-violet-100'
+    >
       &#10554; Go back
     </button>
   );
