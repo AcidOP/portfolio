17d7fab1c210b0c3eb4ff460a37139cbc289efbb
AcidOP <zeeshanalivr46@gmail.com>
2024-04-01 09:30:39 +0530

    build: changed blog card

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,5 @@
 /** @type {import('next').NextConfig} */
 
-import analyzer from '@next/bundle-analyzer';
-
 // const withBundleAnalyzer = import('@next/bundle-analyzer')({
 //   enabled: process.env.ANALYZE === 'true',
 // })
@@ -18,4 +16,4 @@ const nextConfig = {
   },
 };
 
-export default analyzer(nextConfig);
+export default nextConfig;
--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -7,8 +7,8 @@ import Image from 'next/image';
 import Link from 'next/link';
 
 interface BlogPostProps {
-  date: Date;
   slug: string;
+  date: string;
   title: string;
   cover: string;
   description: string;
@@ -16,40 +16,48 @@ interface BlogPostProps {
 
 const BlogCard: FC<BlogPostProps> = ({
   date,
-  description,
   slug,
+  description,
   title,
   cover,
 }) => {
   return (
-    <div className='m-2 rounded-md bg-violet-100 p-3'>
+    <div className='m-2 flex flex-col justify-between rounded-md bg-violet-100 p-3'>
       <Image
         src={cover}
         alt={title}
         width={1920}
         height={1080}
-        className='aspect-video rounded-md'
+        className='aspect-video rounded-xl'
       />
 
-      <h1 className='mt-4 line-clamp-2 text-3xl font-bold'>
+      <h1 className='mt-4 line-clamp-2 text-3xl font-extrabold'>
         <Link href={`/blogs/${slug}`}>{title}</Link>
       </h1>
 
-      <p className='my-3'>{format(date, 'yyyy-MMM-dd-E')}</p>
+      <p className='my-3 text-sm opacity-60'>
+        {format(date, 'yyyy-MMM-dd-E')}
+      </p>
 
       <p className='line-clamp-4'>{description}</p>
+
+      <Link
+        href={`/blogs/${slug}`}
+        className='mb-2 mt-6 w-min text-nowrap text-violet-600 hover:underline'
+      >
+        Read more
+      </Link>
     </div>
   );
 };
 
-const BlogPage: FC<BlogPostProps> = () => {
+const BlogPage = () => {
   const blogs = allBlogsMeta();
+
   return (
     <div className='grid grid-cols-4'>
       {blogs.map(blog => {
-        return (
-          <BlogCard key={blog.slug} {...blog.data} slug={blog.slug} />
-        );
+        return <BlogCard key={blog.data.slug} {...blog.data} />;
       })}
     </div>
   );
