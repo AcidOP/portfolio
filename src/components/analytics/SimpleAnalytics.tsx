14681446f52c28ea925dc5754de1e6b43344b84c
AcidOP <zeeshanalivr46@gmail.com>
2024-05-09 01:00:58 +0530

    fix: ssl broke

--- src/components/analytics/SimpleAnalytics.tsx
+++ src/components/analytics/SimpleAnalytics.tsx
@@ -1,18 +1,18 @@
-import Script from 'next/script';
+import Script from "next/script";
 
 const SimpleAnalytics = () => {
   return (
     <>
       {/* Track pages */}
       <Script
-        strategy='lazyOnload'
-        src='https://api.acidop.codes/latest.js'
+        strategy="lazyOnload"
+        src='https://scripts.simpleanalyticscdn.com/latest.js'
       />
 
       {/* Track events */}
       <Script
-        strategy='lazyOnload'
-        src='https://api.acidop.codes/auto-events.js'
+        strategy="lazyOnload"
+        src="https://scripts.simpleanalyticscdn.com/auto-events.js"
       />
     </>
   );
