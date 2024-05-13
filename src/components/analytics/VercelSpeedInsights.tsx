6626ab1f5e7d5b824f5ee2c56970ebb6431cdb36
AcidOP <zeeshanalivr46@gmail.com>
2024-05-13 10:43:09 +0530

    feat: vercel speed insights added

--- src/components/analytics/VercelSpeedInsights.tsx
+++ src/components/analytics/VercelSpeedInsights.tsx
@@ -1,11 +1,11 @@
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 const VercelSpeedInsights = () => {
   const isEnabled = process.env.VERCEL_SPEED_INSIGHTS === 'true';
 
   if (!isEnabled) return null;
 
-  return <SpeedInsights />
-}
+  return <SpeedInsights />;
+};
 
-export default VercelSpeedInsights
+export default VercelSpeedInsights;
