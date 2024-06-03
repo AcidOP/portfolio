8b0748c19d9f8b285834f05cadc1b7a9c0c0e83a
AcidOP <zeeshanalivr46@gmail.com>
2024-06-03 23:21:00 +0530

    fix: removed misc links from navigation section

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -18,24 +18,24 @@ const Footer = () => {
 
       <div className='my-24 flex flex-col lg:flex-row'>
         <div className='max-w-md'>
-          <h4
+          <h3
             className={cn('mb-6 text-3xl font-bold', suez_400.className)}
           >
             Newsletter 📰
-          </h4>
+          </h3>
           <Newsletter />
         </div>
 
         <div className='mx-auto mt-16 flex w-full justify-between lg:mt-0 lg:w-1/2'>
           <div className='w-full'>
-            <h3 className={cn('text-xl', suez_400.className)}>
+            <h4 className={cn('text-xl', suez_400.className)}>
               Navigation
-            </h3>
+            </h4>
 
             <hr className='my-6 w-[80%] border border-gray-300' />
 
-            <ul className='mt-6 space-y-2'>
-              {links.map(link => (
+            <ul className='space-y-2'>
+              {links.filter(({ dropdown }) => !dropdown).map(link => (
                 <li key={link.url}>
                   <Link href={link.url} className='capitalize'>
                     {link.text}
@@ -46,11 +46,11 @@ const Footer = () => {
           </div>
 
           <div className='w-full'>
-            <h3 className={cn('text-xl', suez_400.className)}>Socials</h3>
+            <h4 className={cn('text-xl', suez_400.className)}>Socials</h4>
 
             <hr className='my-6 w-[80%] border border-gray-300' />
 
-            <ul className='mt-6 space-y-2'>
+            <ul className='space-y-2'>
               {config.socials.map(social => (
                 <li key={social.name}>
                   <Link href={social.url} className='capitalize'>
@@ -62,14 +62,22 @@ const Footer = () => {
           </div>
 
           <div className='w-full'>
-            <h3 className={cn('text-xl', suez_400.className)}>Misc</h3>
+            <h4 className={cn('text-xl', suez_400.className)}>Misc</h4>
 
             <hr className='my-6 w-[80%] border border-gray-300' />
 
-            <ul className='mt-6 space-y-2'>
+            <ul className='space-y-2'>
               <li>
                 <Link href='/privacy-policy'>Privacy Policy</Link>
               </li>
+
+              {links.filter(({ dropdown }) => dropdown).map(link => (
+                <li key={link.url}>
+                  <Link href={link.url} className='capitalize'>
+                    {link.text}
+                  </Link>
+                </li>
+              ))}
             </ul>
           </div>
         </div>
