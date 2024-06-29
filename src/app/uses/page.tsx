4ff72d89393c057b89936d9b62f9b0cb9acb310c
AcidOP <zeeshanalivr46@gmail.com>
2024-06-29 11:46:26 +0530

    refactor: utility files

--- src/app/uses/page.tsx
+++ src/app/uses/page.tsx
@@ -1,23 +1,11 @@
-import ProductCard from '@/components/cards/ProductCard';
+import ListUses from '@/components/lists/ListUses';
 import uses from '@/data/uses';
 
 const Uses = () => {
   return (
     <>
       <h1 className='sr-only'>Uses</h1>
-      {Object.keys(uses).map(category => (
-        <div key={category}>
-          <h2 className='mt-8 text-5xl font-semibold underline'>
-            {category}
-          </h2>
-
-          <div className='my-16 flex flex-wrap'>
-            {uses[category].map(item => (
-              <ProductCard key={item.product} {...item} />
-            ))}
-          </div>
-        </div>
-      ))}
+      <ListUses uses={uses} />
     </>
   );
 };
---
+++ src/components/lists/ListUses.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import ProductCard from '../cards/ProductCard';
+
+import { Uses } from '@/data/uses';
+
+const ListUses = ({ uses }: { uses: Uses }) => {
+  return Object.keys(uses).map(category => (
+    <div key={category}>
+      <h2 className='mt-8 text-5xl font-semibold'>{category}:</h2>
+
+      <div className='my-16 flex flex-wrap'>
+        {uses[category].map(item => (
+          <ProductCard key={item.product} {...item} />
+        ))}
+      </div>
+    </div>
+  ));
+};
+
+export default ListUses;
