674f13e5cf99a378b7950f5d73db08c6e6f9d932
AcidOP <zeeshanalivr46@gmail.com>
2024-10-06 20:39:32 +0530

    fix: responsive view

--- src/components/about-hero.tsx
+++ src/components/about-hero.tsx
@@ -12,20 +12,20 @@ const Hero = () => {
 
   return (
     <>
-      <div className='mb-16'>
+      <div className='mb-16 overflow-hidden'>
         <ScreenFitText text='More' className='lg:hidden' />
         <ScreenFitText text='About Me' />
       </div>
 
-      <div className='mx-auto grid max-w-xl gap-2 lg:max-w-none lg:grid-cols-5'>
-        <section className='pb-16 pt-8 text-4xl font-medium lg:col-span-2 lg:py-0 lg:text-5xl'>
-          <div className='space-y-2 font-medium'>
-            <p>Who TF am I?</p>
-            <p>How I got here?</p>
-            <p>My hobbies?</p>
+      <div className='mx-auto grid max-w-xl gap-2 sm:max-w-none sm:grid-cols-5'>
+        <section className='col-span-5 pb-16 pt-8 text-3xl font-medium sm:col-span-2 md:text-4xl lg:py-0'>
+          <div className='space-y-2 text-gray-600 md:font-bold'>
+            <p>— Who TF am I?</p>
+            <p>— How I got here?</p>
+            <p>— My hobbies?</p>
           </div>
 
-          <p className='my-8 w-min text-nowrap text-white lg:my-16'>
+          <p className='my-8 w-min text-nowrap text-4xl text-white sm:text-3xl md:text-4xl lg:my-16'>
             <RoughNotation
               type='highlight'
               color='#6d28d9'
@@ -37,7 +37,7 @@ const Hero = () => {
           </p>
         </section>
 
-        <section className='relative h-64 border-2 border-black drop-shadow-2xl grayscale transition-all duration-200 md:h-80 lg:col-span-3 lg:h-[30rem]'>
+        <section className='relative col-span-5 h-64 border-2 border-black drop-shadow-2xl grayscale transition-all duration-200 sm:col-span-3 md:h-80 lg:h-[30rem]'>
           <Image
             src='/images/me.jpg'
             fill
@@ -50,7 +50,7 @@ const Hero = () => {
           {isValidTimezone && <TimeDisplay timezone={timezone ?? ''} />}
         </section>
 
-        <section className='text-sm opacity-70 lg:col-span-3 lg:text-2xl'>
+        <section className='col-span-5 text-sm opacity-70 lg:col-span-3 lg:text-2xl'>
           &rarr; A mirror selfie in January 2024
         </section>
 
